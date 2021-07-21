d0c60abf0ca425deec119432c153c01da8e10232
Swagatika Behera <swagatikabehera2001@gmail.com>
2021-07-21 11:45:35 +0530

    hooks

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -1,55 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-class User extends React.Component {
-  constructor(props) {
-    super(props);
+const User = (props) => {
+  const [planet, setPlanet] = useState("Earth");
 
-    this.state = {
-      planet: "earth", //data only User component can hold,no other component can access this data
-    };
-    console.log("Hye I'm from Constructor");
-  }
+  // componentDidMount
+  useEffect(() => {
+    console.log("byeeee");
+    console.log("hho");
+    //componentWillUnmount
+    return console.log("bye");
+  }, []);
 
-  componentDidMount() {
-    this.setState({ planet: "saturn" });
-  }
-  //static getDerivedStateFromProps(prop, state) {
-  //console.log("I'm from get derived state from props");
-  // return { planet: "Jupiter" };
-  //}
+  // componentDidUpdate
+  // shouldComponenetUpdate
+  useEffect(() => {
+    console.log("planet changed");
+  }, [planet]);
 
-  shouldComponentUpdate(nextProp, nextState) {
-    console.log(" from shouldComponentUpdate");
-    console.log({
-      nextProp,
-      nextState,
-    });
-    return true;
-  }
-
-  getSnapshotBeforeUpdate(prevprop, prevState) {
-    console.log(" from getSnapshotBeforeUpdate");
-    console.log({
-      prevprop,
-      prevState,
-    });
-    return true;
-  }
-
-  componentDidUpdate() {
-    console.log(this.state);
-  }
-
-  render() {
-    console.log("Hey I'm from Render()");
-    return (
-      <div>
-        <h1> {this.props.name} </h1>
-        <p> {this.props.description} </p>
-        <h3> {this.state.planet} </h3>
-      </div>
-    );
-  }
-}
+  console.log(planet);
+  return (
+    <div>
+      <h1> {props.name} </h1>
+      <p> {props.description} </p>
+      <button onClick={() => setPlanet("neptune")}>{planet}</button>
+    </div>
+  );
+};
 
 export default User;
